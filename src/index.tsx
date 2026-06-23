221e79d725ad33845e804a52e0af75c9b9132cfa
大見唯希 <dajianweixi@gmail.com>
2026-06-23 18:25:59 +0900

    chore: app.jsキャッシュバスター更新 v6→v8

--- src/index.tsx
+++ src/index.tsx
@@ -400,7 +400,7 @@ const indexHtml = `<!DOCTYPE html>
 
 <div id="toast-container" class="fixed top-4 right-4 z-[100] space-y-2 pointer-events-none"></div>
 
-<script src="/static/app.js?v=6"></script>
+<script src="/static/app.js?v=8"></script>
 </body>
 </html>`
 
