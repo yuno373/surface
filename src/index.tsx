398b33409ecbaf5a6433538ccbae017c85bc8af9
大見唯希 <dajianweixi@gmail.com>
2026-06-27 00:13:49 +0900

    feat: show earthquake time (HH:MM) and ensure TV display shows quake info on open

--- src/index.tsx
+++ src/index.tsx
@@ -556,7 +556,7 @@ const indexHtml = `<!DOCTYPE html>
 
 <div id="toast-container" class="fixed top-4 right-4 z-[100] space-y-2 pointer-events-none"></div>
 
-<script src="/static/app.js?v=15"></script>
+<script src="/static/app.js?v=17"></script>
 </body>
 </html>`
 
